122bcee739cb84a82ec0ed3f8872ee498e8c1dc9
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-21 17:23:58 -0500

    added api app added GiftRecipient model and migrated it

--- secretsanta/api/models.py
+++ secretsanta/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class GiftRecipient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    street_address = models.TextField()
+    city = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
