cfca2317ce75b0b210e6aca81984bb07cd0a2079
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-21 17:46:46 -0500

    added basic template to prove urls mapping function

--- secretsanta/api/views.py
+++ secretsanta/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import GiftRecipientForm
 
 # Create your views here.
+def GiftRecipientEntry(request):
+    if request.method == 'GET':
+        return render(request, 'api/gift_recipient_entry.html')
+    if request.method == 'POST':
+        return render(request, 'api/gift_recipient_entry.html')
