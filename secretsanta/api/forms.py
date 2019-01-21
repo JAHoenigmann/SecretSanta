b9090e601e9be9a2da543bf4b67852d0916fb6d9
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-21 17:29:11 -0500

    added ModelForm for

---
+++ secretsanta/api/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import GiftRecipient
+
+class GiftRecipientForm(ModelForm):
+    class Meta:
+        model = GiftRecipient
+        fields = ['first_name', 'last_name', 'email', 'street_address', 'city',
+                  'zip_code']
