d6e24382caa41486978efcc631b6071f502a99bf
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-03-29 19:33:16 -0400

    stating set up for heroku deployment

--- secretsanta/secretsanta/settings.py
+++ secretsanta/secretsanta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,11 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hoenigmannmolina.secretsanta@gmail.com'
 EMAIL_HOST_PASSWORD = 'Lasvega5s3creT'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
